fd7da91055b3c3f2cf4965c2702c3cf5c9330038
Petru Rares Sincraian <psincraian@outlook.com>
2016-05-17 15:50:43 +0200

    Add basic menu template

--- app/src/main/java/com/psincraian/gameslibrary/MainActivity.java
+++ app/src/main/java/com/psincraian/gameslibrary/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_games) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_characters) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_missions) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_objects) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_options) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
